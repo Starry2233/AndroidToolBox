2d772c729e015fead255e86717090aaae284fb86
HIbingAI <a123459814@outlook.com>
2026-01-12 21:16:05 +0800

    去除失败提示

--- src/start.py
+++ src/start.py
@@ -884,7 +884,7 @@ def pre_main() -> bool:
                     subprocess.run(["cmd", "/c", "start", "repair.exe"])
                     cleanup(2)
         except Exception as e:
-            print_formatted_text(HTML(warn + f"漏洞补丁获取失败，已经跳过"))
+#            print_formatted_text(HTML(warn + f"漏洞补丁获取失败，已跳过"))
         run("call upall.bat run")
     if os.getenv("ATB_SKIP_PLATFORM_CHECK", "0") != "1":
         print_formatted_text(HTML(info + "正在检查Windows属性..."), style=style)
