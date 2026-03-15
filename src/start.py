359dcde8969cef105c4197e7ca7e3b2561708265
Starry2233 <qu20121118@yeah.net>
2026-03-15 16:50:42 +0800

    fix: enable colorama conversion for improved color display

--- src/start.py
+++ src/start.py
@@ -1430,7 +1430,7 @@ def pre_main() -> bool:
     global logger
     global DEBUG
     global CURRENT_BUILD_META
-    colorama.init(autoreset=True)
+    colorama.init(autoreset=True, convert=True)
     run("call .\\color.bat")
     _PATHEXT: str = os.environ["PATHEXT"]
     if not _PATHEXT.endswith(';'): _PATHEXT += ';'
